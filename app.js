2aa630b065a86fb5f44173ac7f657c23bd9dc702
yixinman <Shuhan_Zhang122@163.com>
2016-07-03 15:48:20 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-﻿var http = require("http");
+var http = require("http");
 http.createServer(function(request, response) {
     response.writeHead(200, {"Content-Type": "text/html"});
-    response.write("爸爸我回来啦！");
+    response.write("Hello World!");
     response.end();
 }).listen(8000);

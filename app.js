fbb8fd40b7dabddfed52374dc4a79db0e24823b1
ysYvonne <charlottejo@hotmail.com>
2016-07-03 16:55:18 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var http = require("http");
 http.createServer(function(request, response) {
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.write("Hello World!!");
+    response.writeHead(200, {"Content-Type": "text/html;charset=utf-8";pageEncoding="utf-8"});
+    response.write("王爸爸回来啦！");
     response.end();
 }).listen(8000);
